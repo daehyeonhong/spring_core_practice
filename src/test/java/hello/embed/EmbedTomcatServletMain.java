25b1097808c550955c44ee0c67df8c8172436745
DaeHyeonHong <fholng@outlook.com>
2023-03-11 13:08:40 +0900

    embedTomcat

---
+++ src/test/java/hello/embed/EmbedTomcatServletMain.java
@@ -0,0 +1,26 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbedTomcatServletMain {
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatServletMain.main");
+
+        // tomcat 설정
+        final Tomcat tomcat = new Tomcat();
+        final Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // servlet 연결
+        final Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "helloServlet", new HelloServlet());
+
+        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+        tomcat.start();
+    }
+}
